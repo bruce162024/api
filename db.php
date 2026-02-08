be190b249e0804b3d4f144ef27c2c6f59e040540
bruce162024 <brucedimaano@gmail.com>
2026-02-08 23:52:09 +0800

    Delete database connection code
    Remove database connection code from db.php

--- db.php
+++ db.php
@@ -1,14 +1,1 @@
-<?php
-$host = "sql105.infinityfree.com";   // OR your Render DB host
-$user = "if0_41089035";
-$pass = "7jE0zv5H2C";
-$db   = "if0_41089035_irrs_db";
 
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    http_response_code(500);
-    echo json_encode(["error" => "DB connection failed"]);
-    exit;
-}
-?>
